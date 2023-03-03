e71852f503e6640b2370d176a63eefab886f5f16
kraggio <keilyraggio18@gmail.com>
2023-03-02 21:07:39 -0500

    API IS WORKING

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 const { engine } = require("express-handlebars");
-const require = require("request");
+const request = require("request");
 const axios = require("axios");
-
+//HTTP REQUEST
+const app = express();
 //creates a port for our server
 const PORT = process.env.PORT || 5501;
 
-// set middleware / use the documentation
+// // set middleware / use the documentation
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
-// app.set("views", "./templates/views"); ?????
+app.set("views", "../templates/views");
 
 //make a request API
-//api key:bfc8ea30-60c1-4103-ac32-78bf8f414848
-const encodedParams = new URLSearchParams();
-encodedParams.append("state", "North Carolina");
-encodedParams.append("apiKey", "bfc8ea30-60c1-4103-ac32-78bf8f414848");
-encodedParams.append("country", "United States");
-encodedParams.append("city", "Charlotte");
+//air visual api key:bfc8ea30-60c1-4103-ac32-78bf8f414848
+//rapidAPIkey : da426e0863msh83991ba939afbd1p15de08jsn98dd0281f097
 
-const options = {
-  method: "POST",
-  url: "https://airvisualraygorodskijv1.p.rapidapi.com/getCity",
-  headers: {
-    "content-type": "application/x-www-form-urlencoded",
-    "X-RapidAPI-Key": "da426e0863msh83991ba939afbd1p15de08jsn98dd0281f097",
-    "X-RapidAPI-Host": "AirVisualraygorodskijV1.p.rapidapi.com",
-  },
-  data: encodedParams,
-};
+// const url =
+//   "https://api.airvisual.com/v2/city?city=Charlotte&state=North%20Carolina&country=USA&key=bfc8ea30-60c1-4103-ac32-78bf8f414848";
+
+//lots of inconsistencies with the code below, along w the formatting. typos in previous URLv
+
+const url = new URL("http://api.airvisual.com/v2/city");
+url.searchParams.set("city", "Charlotte");
+url.searchParams.set("state", "North Carolina");
+url.searchParams.set("country", "USA");
+url.searchParams.set("key", "bfc8ea30-60c1-4103-ac32-78bf8f414848");
+
+console.log(url);
+// const encodedParams = new URLSearchParams();
+// encodedParams.append("state", "North Carolina");
+// encodedParams.append("apiKey", "bfc8ea30-60c1-4103-ac32-78bf8f414848");
+// encodedParams.append("country", "USA");
+// encodedParams.append("city", "Charlotte");
+
+// const options = {
+//   method: "POST", in this line it should be "GET"
+//   url: "http://api.airvisual.com/v2",
+//   // headers: {
+//   //   // "content-type": "application/x-www-form-urlencoded",
+//   //   // "X-RapidAPI-Key": "da426e0863msh83991ba939afbd1p15de08jsn98dd0281f097",
+//   //   // "X-RapidAPI-Host": "AirVisualraygorodskijV1.p.rapidapi.com",
+//   // },
+//   // data: encodedParams,
+// };
 
 axios
-  .request(options)
+  .get(url)
   .then(function (response) {
-    console.log(response.data);
+    console.log(response.data.data);
   })
   .catch(function (error) {
     console.error(error);
